a6733fa50ac35caf60c9fe3b81963cea6703e374
Habasque-Mateo <sheolyo@gmail.com>
2021-04-19 11:59:22 +0200

    test post product2

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -6,6 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
 
 class CatalogController
 {
