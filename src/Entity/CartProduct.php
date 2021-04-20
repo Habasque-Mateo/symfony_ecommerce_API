6a50657de658cf017f6d1404be9b1998d3b871d9
Habasque-Mateo <32383496+Habasque-Mateo@users.noreply.github.com>
2021-04-20 15:40:10 +0200

    Update CartProduct.php

--- src/Entity/CartProduct.php
+++ src/Entity/CartProduct.php
@@ -13,14 +13,14 @@ class CartProduct
     /**
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity=Cart::class, inversedBy="cartProducts")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(name="cartId", referencedColumnName="id", nullable=false)
      */
     private $cartId;
 
     /**
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity=Catalog::class, inversedBy="cartProducts")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(name="productId", referencedColumnName="id", nullable=false)
      */
     private $productId;
 
