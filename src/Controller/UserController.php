a08667721831dfe6cadbc3cd7f56994b67a854ab
Habasque-Mateo <32383496+Habasque-Mateo@users.noreply.github.com>
2021-04-27 15:27:08 +0200

    routes users

---
+++ src/Controller/UserController.php
@@ -0,0 +1,76 @@
+<?php 
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+
+class CatalogController
+{
+    private $userRepository;
+    public function __construct(userRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/api/user", name="get_user", methods={"GET"})
+     */
+    public function get(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        if(empty($data['userLogin']))
+        {
+            return new JsonResponse(["error" => "Missing required parameter."], 400);
+        }
+
+        $user = $this->userRepository->findOneByLogin($data['userLogin']);
+
+        if(empty($user))
+        {
+            return new JsonResponse(["error" => "User not found."], 400);
+        }
+
+        $retUser = [
+            "login"=> $user->getLogin(),
+            "email"=> $user->getEmail(),
+            "firstname"=> $user->getFirstname(),
+            "lastname"=> $user->getLastname()
+        ];
+
+        return new JsonResponse($retUser, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/api/user", name="update_user", methods={"PUT"})
+     */
+    public function update(Request $request): JsonResponse
+    {
+        if(empty($data['userLogin']))
+        {
+            return new JsonResponse(["error" => "Missing required parameter."], 400);
+        }
+
+        $user = $this->userRepository->findOneByLogin($data['userLogin']);
+
+        if(empty($user))
+        {
+            return new JsonResponse(["error" => "User not found."], 400);
+        }
+
+        empty($data['userLogin']) ? true : $user->setLogin($data['userLogin']);
+        empty($data['password']) ? true : $user->setPassword($data['password']);
+        empty($data['email']) ? true : $user->setEmail($data['email']);
+        empty($data['firstname']) ? true : $user->setFirstname($data['firstname']);
+        empty($data['lastname']) ? true : $user->setLastname($data['lastname']);
+
+        $updatedUser = $this->userRepository->updateUser($user);
+
+        return new JsonResponse($updatedUser->toArray(), Response::HTTP_OK);
+    }
+}
+
+?>
