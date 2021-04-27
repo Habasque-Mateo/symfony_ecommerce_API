bbb5b2d481bc07b12096126915f3162df7a0d1a0
Habasque-Mateo <32383496+Habasque-Mateo@users.noreply.github.com>
2021-04-27 15:29:55 +0200

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,10 +5,9 @@ use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
-class CatalogController
+class UserController
 {
     private $userRepository;
     public function __construct(userRepository $userRepository)
